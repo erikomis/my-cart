0898a436b428c55bd6d2cf5b316af0ff393e1b50
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-03 02:50:11 -0300

    feat:  use  cart Provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./routes";
 import { RouterProvider } from "react-router-dom";
+import { CartProvider } from "./context/cart";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </React.StrictMode>
 );
