80fe799d45732ca359a6f670303932a41c67d380
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-03 03:14:23 -0300

    feat: use cart in page home e product details  metodos update and remove

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,11 +2,17 @@ import { Container, Grid } from "@mui/material";
 import { Header } from "../../components/header/Header";
 import { CardCustom } from "../../components/cardCustom/CardCustom";
 import { useCart } from "../../context/cart";
-import { CartItemsAmount } from "../../types/cart";
+import { CartItemsAmount, ProductFormatted } from "../../types/cart";
 import { useCallback, useMemo } from "react";
+import Modal from "../../components/modal/Modal";
+import { useModal } from "../../hooks/useModal";
+import { Cart } from "../../components/cart/Cart";
+import { formatPrice } from "../../utils/format";
 
 export const Home = () => {
-  const { addProduct, cart, products } = useCart();
+  const { addProduct, cart, products, removeProduct, updateProductAmount } =
+    useCart();
+  const { open, handleClose, handleOpen } = useModal();
 
   const cartItemsAmount = useMemo(() => {
     return cart?.reduce((sumAmount, product) => {
@@ -22,10 +28,51 @@ export const Home = () => {
     },
     [addProduct]
   );
+  const cartFormatted = useMemo(() => {
+    return cart.map((product) => ({
+      ...product,
+      precoFormatado: formatPrice(product.preco),
+      subTotal: formatPrice(product.preco * product.quantidade),
+    }));
+  }, [cart]);
 
+  const total = useMemo(() => {
+    return formatPrice(
+      cart.reduce((sumTotal, product) => {
+        return sumTotal + product.preco * product.quantidade;
+      }, 0)
+    );
+  }, [cart]);
+
+  const handleProductIncrement = useCallback(
+    (product: ProductFormatted) => {
+      updateProductAmount({
+        productId: product.id,
+        amount: product.quantidade + 1,
+      });
+    },
+    [updateProductAmount]
+  );
+
+  const handleProductDecrement = useCallback(
+    (product: ProductFormatted) => {
+      updateProductAmount({
+        productId: product.id,
+        amount: product.quantidade - 1,
+      });
+    },
+    [updateProductAmount]
+  );
+
+  const handleRemoveProduct = useCallback(
+    (productId: number) => {
+      removeProduct(productId);
+    },
+    [removeProduct]
+  );
   return (
     <Container>
-      <Header />
+      <Header handleOpenCart={handleOpen} />
       <Grid container spacing={4} sx={{ mt: 4 }}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4}>
@@ -38,6 +85,24 @@ export const Home = () => {
           </Grid>
         ))}
       </Grid>
+
+      {open && (
+        <Modal
+          open={open}
+          handleCloseModal={handleClose}
+          title="Carrinho"
+          maxWidth="md"
+          isTitleCenter
+        >
+          <Cart
+            cartFormatted={cartFormatted}
+            handleProductDecrement={handleProductDecrement}
+            handleProductIncrement={handleProductIncrement}
+            handleRemoveProduct={handleRemoveProduct}
+            total={total}
+          />
+        </Modal>
+      )}
     </Container>
   );
 };
--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -1,41 +1,24 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { itemsParaCompra } from "../../utils/utils";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Header } from "../../components/header/Header";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { CardCustom } from "../../components/cardCustom/CardCustom";
 import { useCart } from "../../context/cart";
 import { CartItemsAmount, ProductFormatted } from "../../types/cart";
 import { formatPrice } from "../../utils/format";
+import { useModal } from "../../hooks/useModal";
+import { Cart } from "../../components/cart/Cart";
+import Modal from "../../components/modal/Modal";
 
 export const ProductDetail = () => {
-  const { addProduct, cart } = useCart();
-
   const [product, setProduct] = useState<ProductFormatted>(
     {} as ProductFormatted
   );
+  const { handleClose, handleOpen, open } = useModal();
+  const { addProduct, cart, updateProductAmount, removeProduct } = useCart();
   const { id } = useParams();
-  const handleExits = () => {
-    const productExists = itemsParaCompra.find(
-      (product) => product.id === Number(id)
-    );
-    if (!productExists) {
-      return;
-    }
-
-    setProduct({
-      ...productExists,
-      precoFormatado: formatPrice(productExists.preco),
-    });
-  };
-
-  const cartItemsAmount = useMemo(() => {
-    return cart?.reduce((sumAmount, product) => {
-      const newSumAmount = { ...sumAmount };
-      newSumAmount[product.id] = product.quantidade;
-      return newSumAmount;
-    }, {} as CartItemsAmount);
-  }, [cart]);
+  const navigate = useNavigate();
 
   const handleAddProduct = useCallback(
     (id: number) => {
@@ -44,20 +27,104 @@ export const ProductDetail = () => {
     [addProduct]
   );
 
+  const handleExists = useCallback(() => {
+    const productExists = itemsParaCompra.find(
+      (product) => product.id === Number(id)
+    );
+    if (!productExists) {
+      navigate("/404");
+      return;
+    }
+
+    setProduct({
+      ...productExists,
+      precoFormatado: formatPrice(productExists.preco),
+    });
+  }, [id, navigate]);
+
+  const cartItemsAmount = useMemo(() => {
+    return cart.reduce((sumAmount, product) => {
+      const newSumAmount = { ...sumAmount };
+      newSumAmount[product.id] = product.quantidade;
+      return newSumAmount;
+    }, {} as CartItemsAmount);
+  }, [cart]);
+
   useEffect(() => {
-    handleExits();
-  }, []);
+    handleExists();
+  }, [handleExists]);
+
+  const cartFormatted = useMemo(() => {
+    return cart.map((product) => ({
+      ...product,
+      precoFormatado: formatPrice(product.preco),
+      subTotal: formatPrice(product.preco * product.quantidade),
+    }));
+  }, [cart]);
+
+  const total = useMemo(() => {
+    return formatPrice(
+      cart.reduce((sumTotal, product) => {
+        return sumTotal + product.preco * product.quantidade;
+      }, 0)
+    );
+  }, [cart]);
+
+  const handleProductIncrement = useCallback(
+    (product: ProductFormatted) => {
+      updateProductAmount({
+        productId: product.id,
+        amount: product.quantidade + 1,
+      });
+    },
+    [updateProductAmount]
+  );
+
+  const handleProductDecrement = useCallback(
+    (product: ProductFormatted) => {
+      updateProductAmount({
+        productId: product.id,
+        amount: product.quantidade - 1,
+      });
+    },
+    [updateProductAmount]
+  );
+
+  const handleRemoveProduct = useCallback(
+    (productId: number) => {
+      removeProduct(productId);
+    },
+    [removeProduct]
+  );
 
   return (
     <Container>
-      <Header />
-      {product.id && (
-        <CardCustom
-          product={product}
-          handleAddProduct={handleAddProduct}
-          cartItemsAmount={cartItemsAmount}
-          isDetail
-        />
+      <Header handleOpenCart={handleOpen} />
+      <Typography variant="h3" gutterBottom>
+        Detalhes do Produto
+      </Typography>
+      <CardCustom
+        product={product}
+        handleAddProduct={handleAddProduct}
+        cartItemsAmount={cartItemsAmount}
+        isDetail
+      />
+      {open && (
+        <Modal
+          open={open}
+          handleCloseModal={handleClose}
+          title="Carrinho"
+          maxWidth="md"
+          isTitleCenter
+        >
+          <Cart
+            cartFormatted={cartFormatted}
+            handleProductDecrement={handleProductDecrement}
+            handleProductIncrement={handleProductIncrement}
+            handleRemoveProduct={handleRemoveProduct}
+            total={total}
+          />
+        </Modal>
       )}
     </Container>
   );
