752414d44d2f3431249d3982b356873253b770ab
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-03 02:53:00 -0300

    feat:  add cart

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,40 @@
 import { Container, Grid } from "@mui/material";
 import { Header } from "../../components/header/Header";
 import { CardCustom } from "../../components/cardCustom/CardCustom";
-import { itemsParaCompra } from "../../utils/utils";
+import { useCart } from "../../context/cart";
+import { CartItemsAmount } from "../../types/cart";
+import { useCallback, useMemo } from "react";
 
 export const Home = () => {
+  const { addProduct, cart, products } = useCart();
+
+  const cartItemsAmount = useMemo(() => {
+    return cart?.reduce((sumAmount, product) => {
+      const newSumAmount = { ...sumAmount };
+      newSumAmount[product.id] = product.quantidade;
+      return newSumAmount;
+    }, {} as CartItemsAmount);
+  }, [cart]);
+
+  const handleAddProduct = useCallback(
+    (id: number) => {
+      addProduct(id);
+    },
+    [addProduct]
+  );
+
   return (
     <Container>
       <Header />
       <Grid container spacing={4} sx={{ mt: 4 }}>
-        {itemsParaCompra.map((product) => (
+        {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4}>
-            <CardCustom product={product} />
+            <CardCustom
+              product={product}
+              handleAddProduct={handleAddProduct}
+              cartItemsAmount={cartItemsAmount}
+              isDetail={false}
+            />
           </Grid>
         ))}
       </Grid>
--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react";
-import { Item } from "../../types/items";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { itemsParaCompra } from "../../utils/utils";
 import { useParams } from "react-router-dom";
 import { Header } from "../../components/header/Header";
 import { Container } from "@mui/material";
 import { CardCustom } from "../../components/cardCustom/CardCustom";
+import { useCart } from "../../context/cart";
+import { CartItemsAmount, ProductFormatted } from "../../types/cart";
+import { formatPrice } from "../../utils/format";
 
 export const ProductDetail = () => {
-  const [product, setProduct] = useState<Item>({} as Item);
+  const { addProduct, cart } = useCart();
+
+  const [product, setProduct] = useState<ProductFormatted>(
+    {} as ProductFormatted
+  );
   const { id } = useParams();
   const handleExits = () => {
     const productExists = itemsParaCompra.find(
@@ -19,8 +25,24 @@ export const ProductDetail = () => {
 
     setProduct({
       ...productExists,
+      precoFormatado: formatPrice(productExists.preco),
     });
   };
+
+  const cartItemsAmount = useMemo(() => {
+    return cart?.reduce((sumAmount, product) => {
+      const newSumAmount = { ...sumAmount };
+      newSumAmount[product.id] = product.quantidade;
+      return newSumAmount;
+    }, {} as CartItemsAmount);
+  }, [cart]);
+
+  const handleAddProduct = useCallback(
+    (id: number) => {
+      addProduct(id);
+    },
+    [addProduct]
+  );
 
   useEffect(() => {
     handleExits();
@@ -29,7 +51,14 @@ export const ProductDetail = () => {
   return (
     <Container>
       <Header />
-      {product.id && <CardCustom product={product} />}
+      {product.id && (
+        <CardCustom
+          product={product}
+          handleAddProduct={handleAddProduct}
+          cartItemsAmount={cartItemsAmount}
+          isDetail
+        />
+      )}
     </Container>
   );
 };
