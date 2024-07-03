d7ead4cae18edf345871e697d6f7039127b80a7e
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-03 01:58:23 -0300

    feat:  create route product detail  and  page product detail

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/home/Home";
+import { ProductDetail } from "./pages/productDetail/ProductDetail";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/produto-detail/:id",
+    element: <ProductDetail />,
   },
 ]);
