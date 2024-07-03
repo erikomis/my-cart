1f8e295f2586096b75eb8de9046cb1c89f360dd1
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-03 02:04:54 -0300

    feat:  use card Custom in  prodoct detail

--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { Item } from "../../types/items";
+import { itemsParaCompra } from "../../utils/utils";
+import { useParams } from "react-router-dom";
+import { Header } from "../../components/header/Header";
+import { Container } from "@mui/material";
+import { CardCustom } from "../../components/cardCustom/CardCustom";
+
 export const ProductDetail = () => {
+  const [product, setProduct] = useState<Item>({} as Item);
+  const { id } = useParams();
+  const handleExits = () => {
+    const productExists = itemsParaCompra.find(
+      (product) => product.id === Number(id)
+    );
+    if (!productExists) {
+      return;
+    }
+
+    setProduct({
+      ...productExists,
+    });
+  };
+
+  useEffect(() => {
+    handleExits();
+  }, []);
+
   return (
-    <div>
-      <h1>Product Detail</h1>
-    </div>
+    <Container>
+      <Header />
+      {product.id && <CardCustom product={product} />}
+    </Container>
   );
 };
