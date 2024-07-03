e05c988522913ae1cdd9197e1370db04a3098a2a
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-03 01:54:42 -0300

    feat: mocks and types

---
+++ src/types/items.ts
@@ -0,0 +1,12 @@
+export interface Item {
+  id: number;
+  nome: string;
+  preco: number;
+  descricao: string;
+  quantidade: number;
+}
+export type Estoque = {
+  id: number;
+  quantia: number;
+};
+
---
+++ src/utils/utils.ts
@@ -0,0 +1,122 @@
+import { Estoque, Item } from "../types/items";
+
+
+export const estoque: Estoque[] = [
+  {
+    id: 1,
+    quantia: 3,
+  },
+  {
+    id: 2,
+    quantia: 5,
+  },
+  {
+    id: 3,
+    quantia: 2,
+  },
+  {
+    id: 4,
+    quantia: 1,
+  },
+  {
+    id: 5,
+    quantia: 5,
+  },
+  {
+    id: 6,
+    quantia: 10,
+  },
+  {
+    id: 7,
+    quantia: 4,
+  },
+  {
+    id: 8,
+    quantia: 2,
+  },
+  {
+    id: 9,
+    quantia: 3,
+  },
+  {
+    id: 10,
+    quantia: 5,
+  },
+];
+
+export const itemsParaCompra: Item[] = [
+  {
+    id: 1,
+    nome: "Notebook",
+    preco: 3500.0,
+    descricao: "Notebook de alta performance com 16GB de RAM e 512GB SSD.",
+    quantidade: 0,
+  },
+  {
+    id: 2,
+    nome: "Smartphone",
+    preco: 2500.0,
+    descricao:
+      "Smartphone com tela de 6.5 polegadas, 128GB de armazenamento e câmera de 48MP.",
+    quantidade: 0,
+  },
+  {
+    id: 3,
+    nome: "Fone de Ouvido",
+    preco: 300.0,
+    descricao: "Fone de ouvido sem fio com cancelamento de ruído ativo.",
+    quantidade: 0,
+  },
+  {
+    id: 4,
+    nome: "Monitor",
+    preco: 1200.0,
+    descricao: "Monitor de 27 polegadas com resolução 4K e tecnologia IPS.",
+    quantidade: 0,
+  },
+  {
+    id: 5,
+    nome: "Teclado Mecânico",
+    preco: 450.0,
+    descricao: "Teclado mecânico com switches Cherry MX Blue e iluminação RGB.",
+    quantidade: 0,
+  },
+  {
+    id: 6,
+    nome: "Mouse Gamer",
+    preco: 200.0,
+    descricao: "Mouse gamer com 16000 DPI, iluminação RGB e design ergonômico.",
+    quantidade: 0,
+  },
+  {
+    id: 7,
+    nome: "Cadeira Gamer",
+    preco: 850.0,
+    descricao:
+      "Cadeira gamer com ajuste de altura, reclinação e apoio de braço.",
+    quantidade: 0,
+  },
+  {
+    id: 8,
+    nome: "Impressora Multifuncional",
+    preco: 600.0,
+    descricao: "Impressora multifuncional com scanner e copiadora integrada.",
+    quantidade: 0,
+  },
+  {
+    id: 9,
+    nome: "Tablet",
+    preco: 1500.0,
+    descricao:
+      "Tablet com tela de 10 polegadas, 64GB de armazenamento e caneta stylus.",
+    quantidade: 0,
+  },
+  {
+    id: 10,
+    nome: "Smartwatch",
+    preco: 800.0,
+    descricao:
+      "Smartwatch com monitoramento de atividades, GPS e notificações inteligentes.",
+    quantidade: 0,
+  },
+];
