6ddeb79b5a992fdb399f430231b79357d5304454
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-03 02:50:55 -0300

    feat:  add cart

--- src/components/cardCustom/CardCustom.tsx
+++ src/components/cardCustom/CardCustom.tsx
@@ -3,13 +3,25 @@ import {
   Card,
   CardActions,
   CardContent,
+  IconButton,
   Typography,
 } from "@mui/material";
 
 import { Link } from "react-router-dom";
-import { Item } from "../../types/items";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { CartItemsAmount, ProductFormatted } from "../../types/cart";
 
-export const CardCustom = ({ product }: { product: Item }) => {
+export const CardCustom = ({
+  product,
+  handleAddProduct,
+  cartItemsAmount,
+  isDetail,
+}: {
+  product: ProductFormatted;
+  handleAddProduct: (id: number) => void;
+  cartItemsAmount: CartItemsAmount;
+  isDetail?: boolean;
+}) => {
   return (
     <Card>
       <CardContent>
@@ -17,18 +29,46 @@ export const CardCustom = ({ product }: { product: Item }) => {
           {product.nome}
         </Typography>
         <Typography variant="body2" color="textSecondary">
-          Preço: {product.preco}
+          Preço: {product.precoFormatado}
         </Typography>
+
+        {isDetail && (
+          <Typography variant="body1" color="textSecondary" sx={{ mt: 2 }}>
+            {product.descricao}
+          </Typography>
+        )}
       </CardContent>
       <CardActions>
-        <Button
-          component={Link}
-          to={"/produto-detail/" + product.id}
-          variant="outlined"
-          color="primary"
-        >
-          Ver Detalhes
-        </Button>
+        {isDetail ? (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleAddProduct(product.id)}
+          >
+            Adicionar ao Carrinho ({cartItemsAmount[product.id] || 0})
+          </Button>
+        ) : (
+          <>
+            <Button
+              component={Link}
+              to={"/produto-detail/" + product.id}
+              variant="outlined"
+              color="primary"
+            >
+              Ver Detalhes
+            </Button>
+            <IconButton
+              color="primary"
+              data-testid="add-product-button"
+              onClick={() => handleAddProduct(product.id)}
+            >
+              <AddShoppingCartIcon />
+              <Typography variant="body2" component="span" sx={{ ml: 1 }}>
+                {cartItemsAmount[product.id] || 0}
+              </Typography>
+            </IconButton>
+          </>
+        )}
       </CardActions>
     </Card>
   );
